548c2455b985dfc44017f3addee526082b73b9df
Krystian <krystian.matusiak13@gmail.com>
2023-02-20 18:11:19 +0100

    seaborn crosstab works

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ try:
     import numpy as np
     import tensorflow as tf
     import matplotlib.pyplot as plt
+    import seaborn as sb
 except:
     print("Smth is wrong")
 
@@ -48,12 +49,31 @@ def plot_record(GlobVar):
 
 # ---------------------------------------------------------------------------------
 # Print crosstab
-def print_crosstab(X_test, Y_test, model):
+def print_crosstab_terminal(X_test, Y_test, model):
     y_true = tf.argmax(Y_test, 1)
     y_pred = tf.argmax(model.predict(X_test), 1)
     results = pd.crosstab(index=y_true, columns=y_pred)
     print(results)
 
+def print_crosstab_seaborn(X_test, Y_test, model):
+    labels = Y_test
+    preds = model.predict(X_test)
+    y_preds = tf.argmax(preds, 1).numpy()
+
+    exact_vec = []
+    for y in Y_test:
+        exact_vec.append(y)
+    predict_vec = []
+    for y in y_preds:
+        predict_vec.append(y)
+
+    data = {'Exact_values': exact_vec, "Predictions": predict_vec}
+    df = pd.DataFrame(data=data)
+    results = pd.crosstab(df['Exact_values'],df['Predictions'])
+    plt.figure(figsize=(10,7))
+    sb.heatmap(results, annot=True, cmap="OrRd", fmt=".0f")
+    plt.title("Crosstab for tensorflow")
+    # plt.show()
 
 # ---------------------------------------------------------------------------------
 # Print example predictions
